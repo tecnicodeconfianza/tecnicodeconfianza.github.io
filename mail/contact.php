75e4544da4444f4a4568dd41dd9deba2dd4cfcd9
tecnicodeconfianza <tecnicodeconfianza@hotmail.com>
2026-01-22 09:16:31 +0100

    update

--- mail/contact.php
+++ mail/contact.php
@@ -9,9 +9,9 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "tecnicodeconfianza@hotmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$body = "Has recibido un nuevo mensaje de la página web.\n\n"."Aquí están los detalles:\n\nName: $name\n\n\nEmail: $email\n\nAsunto: $m_subject\n\nMensaje: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
